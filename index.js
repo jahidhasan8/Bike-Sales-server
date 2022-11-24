71be992524d49127e6d40a010b07b2bdddaae44d
jahidhasan8 <jahidhasan1932@gmail.com>
2022-11-24 18:59:50 +0600

    categories api and users api created

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const express = require('express')
 const cors = require('cors')
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
-
-
 const port = process.env.PORT || 5000
 
 const app = express()
@@ -22,13 +20,29 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 async function run(){
     try{
+        const categoriesCollection = client.db('bike-sales').collection('categories')
+        const usersCollection = client.db('bike-sales').collection('users')
 
+        app.get('/categories',async(req,res)=>{
+            const query={}
+            const categories= await categoriesCollection.find(query).toArray()
+            res.send(categories)
+        })
+        app.post('/users', async (req, res) => {
+            const user = req.body
+            
+            const result = await usersCollection.insertOne(user)
+            console.log(result);
+            res.send(result)
+        })
     }
     finally{
-        
+
     }
 }
 
+run()
+.catch(error => console.log(error.message))
 
 app.get('/', async (req, res) => {
     res.send('Bike Sales server is running')
