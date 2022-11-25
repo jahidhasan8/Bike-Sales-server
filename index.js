68815f48437cad18dcbeb3dbd816447b235e8a41
jahidhasan8 <jahidhasan1932@gmail.com>
2022-11-26 03:29:39 +0600

    get api created for bookings

--- index.js
+++ index.js
@@ -53,10 +53,20 @@ async function run(){
             const products = await productsCollection.find(query).toArray()
             res.send(products)
         });
-         
+          
+        app.get('/bookings', async (req, res) => {
+
+            const email = req.query.email
+
+            const query = { 
+                email: email
+            }
+            const bookings = await bookingCollection.find(query).toArray()
+            res.send(bookings)
+        })
+
         app.post('/bookings', async (req, res) => {
             const booking = req.body
-           
             const result = await bookingCollection.insertOne(booking)
             res.send(result)
         });
