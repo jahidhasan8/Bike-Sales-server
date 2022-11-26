dc10e6b5784408282443aff3220c9fe4d7150089
jahidhasan8 <jahidhasan1932@gmail.com>
2022-11-26 23:15:02 +0600

    payment api created

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const cors = require('cors')
 require('dotenv').config()
-const { MongoClient, ServerApiVersion,ObjectId } = require('mongodb');
-const jwt=require('jsonwebtoken');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const jwt = require('jsonwebtoken');
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
 const port = process.env.PORT || 5000
 
 const app = express()
@@ -33,46 +35,35 @@ function verifyJWT(req, res, next) {
     })
 }
 
-async function run(){
-    try{
+async function run() {
+    try {
         const categoriesCollection = client.db('bike-sales').collection('categories')
         const usersCollection = client.db('bike-sales').collection('users')
         const productsCollection = client.db('bike-sales').collection('products')
         const bookingCollection = client.db('bike-sales').collection('bookings')
 
-        app.get('/categories',async(req,res)=>{
-            const query={}
-            const categories= await categoriesCollection.find(query).toArray()
+        app.get('/categories', async (req, res) => {
+            const query = {}
+            const categories = await categoriesCollection.find(query).toArray()
             res.send(categories)
         })
-         
+
         app.get('/products', async (req, res) => {
 
             const email = req.query.email
 
-            const query = { 
-               sellerEmail: email
+            const query = {
+                sellerEmail: email
             }
             const products = await productsCollection.find(query).toArray()
             res.send(products)
         })
-         
-        app.get('/products/advertised',async(req,res)=>{
-            // const advertise=req.query.advertise
-            // console.log(typeof advertise);
-            const query={advertise:true}
-            const advertised=await productsCollection.find(query).toArray()
+
+        app.get('/products/advertised', async (req, res) => {
+            const query = { advertise: true }
+            const advertised = await productsCollection.find(query).toArray()
             res.send(advertised)
         })
-          
-
-        // app.get('/users/admin/:email', async (req, res) => {
-        //     const email = req.params.email
-        //     const query = { email }
-        //     const user = await usersCollection.findOne(query);
-        //     res.send({ isAdmin: user?.role === 'admin' });
-
-        // })
 
         app.get('/products/:id', async (req, res) => {
             const categoryId = req.params.id
@@ -80,12 +71,12 @@ async function run(){
             const products = await productsCollection.find(query).toArray()
             res.send(products)
         });
-          
-        app.get('/bookings',verifyJWT, async (req, res) => {
+
+        app.get('/bookings', verifyJWT, async (req, res) => {
 
             const email = req.query.email
-             const decodedMail=req.decoded.email
-            const query = { 
+            const decodedMail = req.decoded.email
+            const query = {
                 email: email
             }
 
@@ -95,6 +86,14 @@ async function run(){
 
             const bookings = await bookingCollection.find(query).toArray()
             res.send(bookings)
+        })
+
+        app.get('/bookings/:id', async (req, res) => {
+
+            const id = req.params.id
+            const query = { _id: ObjectId(id) }
+            const booking = await bookingCollection.findOne(query)
+            res.send(booking)
         })
 
         app.post('/bookings', async (req, res) => {
@@ -116,27 +115,46 @@ async function run(){
             res.status(403).send({ jwToken: 'forbidden access' })
 
         })
-         
+
         app.post('/products', async (req, res) => {
             const product = req.body
             // console.log(product);
             const result = await productsCollection.insertOne(product)
             res.send(result)
         });
-        
+
+        app.post("/create-payment-intent", async (req, res) => {
+
+             const booking=req.body
+             const price=booking.price
+             const amount=price*100
+
+             const paymentIntent = await stripe.paymentIntents.create({
+                amount: amount,
+                currency: "usd",
+                "payment_method_types": [
+                  "card"
+                ],
+              });
+
+            res.send({
+                clientSecret: paymentIntent.client_secret,
+            });
+        });
+
         app.put('/products/:id', async (req, res) => {
 
-              const id = req.params.id
-              const filter = { _id: ObjectId(id) }
-              const options = { upsert: true }
-              const updatedDoc = {
-                  $set: {
-                      advertise: true
-                  }
-              }
-              const result = await productsCollection.updateOne(filter, updatedDoc, options)
-              res.send(result);
-          });
+            const id = req.params.id
+            const filter = { _id: ObjectId(id) }
+            const options = { upsert: true }
+            const updatedDoc = {
+                $set: {
+                    advertise: true
+                }
+            }
+            const result = await productsCollection.updateOne(filter, updatedDoc, options)
+            res.send(result);
+        });
 
 
         app.get('/users/account/:email', async (req, res) => {
@@ -147,22 +165,22 @@ async function run(){
 
         })
 
-        app.get('/users',async(req,res)=>{
-           
-            let query={} 
-            
-             if (req.query.accountType) {
+        app.get('/users', async (req, res) => {
+
+            let query = {}
+
+            if (req.query.accountType) {
                 query = {
                     accountType: req.query.accountType
                 }
             }
-            const users=await usersCollection.find(query).toArray();
+            const users = await usersCollection.find(query).toArray();
             res.send(users)
         })
-    
+
         app.post('/users', async (req, res) => {
             const user = req.body
-            
+
             const result = await usersCollection.insertOne(user)
             // console.log(result);
             res.send(result)
@@ -170,19 +188,19 @@ async function run(){
 
         app.delete('/users/:id', async (req, res) => {
             const id = req.params.id
-            const filter={_id:ObjectId(id)}
+            const filter = { _id: ObjectId(id) }
             const result = await usersCollection.deleteOne(filter)
             res.send(result)
 
         })
     }
-    finally{
+    finally {
 
     }
 }
 
 run()
-.catch(error => console.log(error.message))
+    .catch(error => console.log(error.message))
 
 app.get('/', async (req, res) => {
     res.send('Bike Sales server is running')
